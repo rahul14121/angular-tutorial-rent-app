8e6143b24d024b6c351bae5e45b4e911c71a58e2
Rahul Patel <rahulpatel@btinternet.com>
2023-09-14 14:12:33 +0100

    Step 6 complete

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,15 +11,15 @@ import { HousingLocation } from '../housinglocation';
     HousingLocationComponent
   ],
   template: `
-    <section>
-    <form>
-      <input type="text" placeholder="Filter by city">
-      <button class="primary" type="button">Search</button>
-    </form>
-  </section>
-  <section class="results">
-    <app-housing-location></app-housing-location>
-  </section>
+     <section>
+      <form>
+        <input type="text" placeholder="Filter by city">
+        <button class="primary" type="button">Search</button>
+      </form>
+    </section>
+    <section class="results">
+      <app-housing-location [housingLocation]="housingLocation"></app-housing-location>
+    </section>
   `,
   styleUrls: ['./home.component.css']
 })
